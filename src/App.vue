a1fbc53691249de481ebfb6dc69673b12227e75e
Erica <erica.du0916@gmail.com>
2021-10-17 13:40:59 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="p-5">
+    <button class="btn">按鈕</button>
+    <button class="
+      bg-purple-500 text-center text-white rounded px-3 py-1 hover:bg-purple-700">按鈕</button>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "./assets/style.css";
 </style>
